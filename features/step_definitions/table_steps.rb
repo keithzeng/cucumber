88aecf0d00e94808a03f68d061ea4a4d0c614f67
keithzeng <shihezeng@gmail.com>
2016-04-02 20:37:49 -0400

    first commit

---
+++ features/step_definitions/table_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a board like this:$/) do |table|
+  @board = table.raw
+end
+
+When(/^player x plays in row (\d+), column (\d+)$/) do |row, col|
+end
+
+
+Then(/^the board should look like this:$/) do |expected_table|
+  expected_table.diff!(@board)
+end
